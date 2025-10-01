0ed76bc484a76c4d81ce2e79f09f136b0ccafa0f
Adi Bal-Mayel <abalmayel@gmail.com>
2025-10-01 12:07:27 -0400

    App testing

---
+++ cars_demo/data.py
@@ -0,0 +1,12 @@
+cars_db = [
+    {"id":"3", "created_at":"2025-10-01 13:15:05+00", "make":"chevrolet", "model": "chevelle malibu", "city": "Toronto"},
+    {"id":"4", "created_at":"2025-09-01 13:15:05+00", "make":"ford", "model": "mustang", "city": "Vancouver"},
+    {"id":"5", "created_at":"2025-04-01 13:15:05+00", "make":"dodge", "model": "charger", "city": "Montreal"},
+    {"id":"6", "created_at":"2025-07-01 13:15:05+00", "make":"pontiac", "model": "gto", "city": "Calgary"},
+    {"id":"7", "created_at":"2025-01-01 13:15:05+00", "make":"chevrolet", "model": "camaro", "city": "Ottawa"},
+    {"id":"8", "created_at":"2025-02-01 13:15:05+00", "make":"plymouth", "model": "road runner", "city": "Edmonton"},
+    {"id":"9", "created_at":"2025-03-28 13:15:05+00", "make":"buick", "model": "gnx", "city": "Winnipeg"},
+    {"id":"10", "created_at":"2025-05-21 13:15:05+00", "make":"mercury", "model": "cougar", "city": "Quebec City"},
+    {"id":"11", "created_at":"2025-09-12 13:15:05+00", "make":"amc", "model": "javelin", "city": "Hamilton"},
+    {"id":"12", "created_at":"2025-10-23 13:15:05+00", "make":"oldsmobile", "model": "cutlass", "city": "Kitchener"}
+]
--- main.py
+++ main.py
@@ -1,9 +1,32 @@
 from supabase import create_client, Client
 from dotenv import load_dotenv
 import os
+from cars_demo.data import cars_db
 
 load_dotenv()
 
 SUPABASE_URL: str = os.getenv("SUPABASE_URL", "supabase project url")
 SUPABASE_KEY: str = os.getenv("SUPABASE_KEY", "supabase project key")
 
+supabase : Client = create_client(SUPABASE_URL, SUPABASE_KEY)
+
+results = supabase.table("cars-demo").select("*").execute()
+
+# for car in cars_db:
+#     if not any(d['id'] == car['id'] for d in results.data):
+#         try:
+#             supabase.table("cars-demo").insert(car).execute()
+#             print(f"Inserted car with id: {car['id']}")
+#         except Exception as e:
+#             print(f"Error inserting car with id: {car['id']}. Error: {e}")
+#     else:
+#         print(f"Car with id: {car['id']} already exists")
+
+## Updating a record
+new_car = {"id":"3", "created_at":"2025-10-01 13:15:05+00", "make":"chevrolet", "model": "chevelle malibu", "city": "Toronto"}
+supabase.table("cars-demo").update(new_car).eq("city", "Ottawa").execute()
+
+## Deleting a record
+# supabase.table("cars-demo").delete().eq("id", 2).execute()
+
+response = supabase.storage().from_("cars-demo").get_public_url("tesla.png")
